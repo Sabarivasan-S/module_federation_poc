d53195f2f109a9582143733fb3c18ef04e651015
Sabarivasan-S <sabarivasan.s@rently.com>
2024-05-14 14:22:11 +0530

    DATA-5461 Initial commit

---
+++ craco.config.js
@@ -0,0 +1,37 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+const deps = require("./package.json").dependencies;
+
+module.exports = () => ({
+  devServer: {
+    port: 3000,
+  },
+  webpack: {
+    configure: {
+      output: {
+        publicPath: "auto",
+      },
+    },
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: "onboarding",
+          filename: "remoteEntry.js",
+          exposes: {
+          },
+          shared: {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: deps["react-dom"],
+            },
+          },
+        }),
+      ],
+    },
+  },
+});
